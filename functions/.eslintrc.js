802db92c10c53daf86d1cb0c2cc368f6161aa32f
Mariana Manzo <manzov.mariana@gmail.com>
2025-10-20 15:00:04 -0600

    chore: align functions tooling for TypeScript linting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,10 +4,17 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:import/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
+    project: ["./tsconfig.json"],
   },
   rules: {
     "import/no-unresolved": "off",
